a777e4e03fe4e86227fd0ddc669e5b39a6d4736a
BRodrigues98 <bruno.m.v.rodrigues1998@gmail.com>
2020-05-07 00:23:40 +0100

    First commit. Only state to implement is GameOver and its corresponding UI function. Needs testing.

--- src/pt/isec/br/TP_PA19_20/Main.java
+++ src/pt/isec/br/TP_PA19_20/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Text ui = new Text(new GameToStates());
+        //Run the program in the Text class of the UI package
         ui.run();
     }
 }
